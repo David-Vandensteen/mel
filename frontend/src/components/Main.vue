24d4aa5ca17d550a64d849158dfd1e03c5978612
David Vandensteen <david.vandensteen@leroymerlin.fr>
2020-01-26 11:22:27 +0100

    vue header, main, footer

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    {{ items }}
     <v-layout
       text-center
       wrap
@@ -85,60 +86,21 @@
 </template>
 
 <script>
+import Explorer from '../class/Explorer';
+
 export default {
   name: 'Main',
 
+  created() {
+    this.explorer = new Explorer();
+    this.explorer.get()
+      .then((response) => {
+        this.items = response.data;
+      });
+  },
+
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+    items: [],
   }),
 };
 </script>
